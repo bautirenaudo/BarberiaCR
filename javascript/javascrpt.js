3d7635e35e3907870f68d5bc5eebb3a1241273f9
bautirenaudo <78118334+bautirenaudo@users.noreply.github.com>
2021-05-30 17:02:37 -0300

    Add files via upload

--- javascript/javascrpt.js
+++ javascript/javascrpt.js
@@ -1,22 +1,23 @@
 (function(){
     //VARIABLES
-    var menu= document.getElementById("icon-menu2"); 
+    var menu= document.getElementById("icon-circle-right"); 
+    var menu2= document.getElementById("icon-circle-left"); 
     var nav2=document.getElementById("nav2");
-    var i=0;
 
     //FUNCIONES
     var abrir= function(){
-        if(i==0){
-          nav2.style.cssText="transform: translateX(0%);"
-          i=1;  
-        }else{
-          nav2.style.cssText="transform: translateX(-100%);"
-          i=0;
-        }
-      
+      nav2.style.cssText="transform: translateX(0%);";  
+      menu2.style.cssText="display: block; opacity: 1;"; 
+      menu.style.cssText="animation: menu .3s linear; opacity: 0;"; 
+    }
+    var cerrar= function(){
+      nav2.style.cssText="transform: translateX(-100%);"; 
+      menu2.style.cssText="opacity: 0;";
+      menu.style.cssText="opacity: 1;"; 
     }
 
     //EVENTOS  
     menu.addEventListener("click",abrir);
+    menu2.addEventListener("click",cerrar);
     
 }());
